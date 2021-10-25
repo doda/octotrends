a87119be0dedf9939826f727619335e2abeec677
Dominik Dabrowski <dominik+github@doda.co>
2021-10-25 15:35:16 -0700

    Remove inner borders from ButtonGroup

--- src/shared/ButtonGroup.js
+++ src/shared/ButtonGroup.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { classNames } from './Utils'
+import React from "react";
+import { classNames } from "./Utils";
 
 export function ButtonGroup({ children, className, ...rest }) {
   return (
@@ -15,19 +15,23 @@ export function ButtonGroup({ children, className, ...rest }) {
     </div>
   );
 }
+
 export function GroupButton({ children, className, dir, ...rest }) {
-    return (
-      <button
-        className={classNames(
-          "border border-blue-500 px-4 py-2 mx-0 outline-none focus:shadow-outline",
-          rest.left ? "border-r-0 rounded-l-sm" : "",
-          rest.right ? "border-l-0 rounded-r-sm" : "",
-          rest.active ? "bg-blue-500 text-white" : "bg-white text-blue-500",
-          className
-        )}
-        {...rest}
-      >
-        {children}
-      </button>
-    );
-  }
+  return (
+    <button
+      className={classNames(
+        "border border-blue-500 px-4 py-2 mx-0 outline-none focus:shadow-outline",
+        rest.left
+          ? "border-r-0 rounded-l-sm"
+          : rest.right
+          ? "border-l-0 rounded-r-sm"
+          : "border-r-0 border-l-0",
+        rest.active ? "bg-blue-500 text-white" : "bg-white text-blue-500",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
