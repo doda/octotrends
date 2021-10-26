c2f3d9fa67ae795ce0a2c77fd777597133145a92
Dominik Dabrowski <dominik+github@doda.co>
2021-10-26 14:52:22 -0700

    Simplify CH query

--- main.go
+++ main.go
@@ -22,100 +22,78 @@ type Period struct {
 type DataTable map[string]TableItem
 
 type TableItem struct {
-	Baseline7  int
-	Added7     int
-	Baseline30 int
-	Added30    int
-	Baseline90 int
-	Added90    int
+	Added7  int
+	Added30 int
+	Added90 int
 }
 
 type JSONOutItem struct {
 	Name        string
 	Stars       int
-	Baseline7   int
 	Added7      int
-	Baseline30  int
 	Added30     int
-	Baseline90  int
 	Added90     int
 	Language    string
 	Topics      string
 	Description string
 }
 
-var RepoSelectQuery = `
-SELECT
-	repo_name
-FROM github_events
-WHERE event_type = 'WatchEvent' AND created_at > minus(now(), toIntervalDay(90))
-GROUP BY repo_name
-HAVING (count() >= ?)
-`
-
 var StarsSelectQuery = `
-WITH dateDiff('day', created_at, (SELECT max(created_at) FROM github_events)) as days
+WITH 
+	(SELECT max(created_at) FROM github_events) as right_now,
+	dateDiff('day', created_at_sub, right_now) as days
 SELECT
 	repo_name,
-	sum(days > ?) as baseline,
-	sum(days <= ?) as added
-FROM github_events
-WHERE event_type = 'WatchEvent'
+	sum(days <= 7) as added7,
+	sum(days <= 30) as added30,
+	sum(days <= 90) as added90
+FROM 
+( // at most 1 star per user per repo
+	SELECT
+		repo_name,
+		actor_login,
+		any(created_at) as created_at_sub
+	FROM github_events
+	WHERE event_type = 'WatchEvent'
+	GROUP BY repo_name, actor_login
+)
+WHERE repo_name in 
+( // "interesting" repos list
+	SELECT
+		repo_name
+	FROM github_events
+	WHERE event_type = 'WatchEvent' AND created_at > minus(right_now, toIntervalDay(90))
+	GROUP BY repo_name
+	HAVING (count() >= ?)
+)
 GROUP BY repo_name
-HAVING repo_name in (` + RepoSelectQuery + `)
 `
 
-// Get an initial list of repos to scrape that meet a minimum popularity bar
-func GetRepos(connect *sqlx.DB, minStars int) DataTable {
-	var items []string
+// Use ClickHouse to quickly aggregate how many people have starred the repo recently
+func GetGrowths(connect *sqlx.DB, minStars int) DataTable {
+	data := DataTable{}
 
-	if err := connect.Select(&items, RepoSelectQuery, minStars); err != nil {
+	var items []struct {
+		RepoName string `db:"repo_name"`
+		Added7   int32  `db:"added7"`
+		Added30  int32  `db:"added30"`
+		Added90  int32  `db:"added90"`
+	}
+
+	fmt.Println("Running", StarsSelectQuery)
+	if err := connect.Select(&items, StarsSelectQuery, minStars); err != nil {
 		log.Fatal(err)
 	}
 
-	data := DataTable{}
-	for _, repoName := range items {
-		data[repoName] = TableItem{}
+	for _, item := range items {
+		dataItem := data[item.RepoName]
+		dataItem.Added7 = int(item.Added7)
+		dataItem.Added30 = int(item.Added30)
+		dataItem.Added90 = int(item.Added90)
+		data[item.RepoName] = dataItem
 	}
-	log.Printf("# repos to scrape: %d", len(items))
+	log.Printf("Aggregated %d items", len(items))
 	return data
-}
-
-// Use ClickHouse to quickly aggregate how many people have starred the repo in the "baseline" (second-to-last) period and the
-// "added" (last) period. This allows us to calculate the % growth within the "added" period.
-func GetGrowths(connect *sqlx.DB, data DataTable, minStars int) {
-	periods := []Period{
-		{"1mo", 7},
-		{"6mo", 30},
-		{"1y", 90},
-	}
-	for _, period := range periods {
-		var items []struct {
-			RepoName string `db:"repo_name"`
-			Baseline int32  `db:"baseline"`
-			Added    int32  `db:"added"`
-		}
-		fmt.Println("Running", StarsSelectQuery)
-		if err := connect.Select(&items, StarsSelectQuery, period.days, period.days, minStars); err != nil {
-			log.Fatal(err)
-		}
-
-		for _, item := range items {
-			dataItem := data[item.RepoName]
-			base, added := int(item.Baseline), int(item.Added)
-			switch period.days {
-			case 7:
-				dataItem.Baseline7, dataItem.Added7 = base, added
-			case 30:
-				dataItem.Baseline30, dataItem.Added30 = base, added
-			case 90:
-				dataItem.Baseline90, dataItem.Added90 = base, added
-			}
-			data[item.RepoName] = dataItem
-		}
-		log.Printf("Aggregated %d items for period %d days", len(items), period.days)
-
-	}
 }
 
 // Write our accumulated & joined information out to JSON for the frontend to consume
@@ -132,11 +110,8 @@ func WriteToJSON(d DataTable, jsonMap map[string]github.Repository, outFileName 
 		outItems = append(outItems, JSONOutItem{
 			Name:        repoName,
 			Stars:       IntValue(repoInfo.StargazersCount),
-			Baseline7:   tableItem.Baseline7,
 			Added7:      tableItem.Added7,
-			Baseline30:  tableItem.Baseline30,
 			Added30:     tableItem.Added30,
-			Baseline90:  tableItem.Baseline90,
 			Added90:     tableItem.Added90,
 			Language:    language,
 			Topics:      strings.Join(repoInfo.Topics, ", "),
@@ -167,21 +142,18 @@ func main() {
 		return
 	}
 
-	log.Println("Getting repos that have received more than", *minStars, "stars in the past", 90, "days, and using ClickHouse URL:", *clickHouseURL)
+	log.Printf("Getting repos that have received more than %d stars in the past 90 days, and using ClickHouse URL: %s", *minStars, *clickHouseURL)
 
 	connect, err := sqlx.Open("clickhouse", *clickHouseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Get repos we want to have
-	dataTable := GetRepos(connect, *minStars)
+	// Get Growth dataTable from ClickHouse
+	dataTable := GetGrowths(connect, *minStars)
 
 	// Get GitHub dataTable for these repos (either cached or anew)
 	GHInfoMap := GetGHRepoInfo(dataTable, *githbToken)
-
-	// Get Growth dataTable from ClickHouse
-	GetGrowths(connect, dataTable, *minStars)
 
 	// Write out
 	WriteToJSON(dataTable, GHInfoMap, *outFileName)
